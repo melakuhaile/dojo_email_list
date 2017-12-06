d507dd83d33b5fe34c76788ec255323d531064ae
melakusejamo <melakusejam@gmail.com>
2017-12-05 20:48:22 -0500

    Dojo_email_list assigment

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ComponentAComponent } from './component-a/component-a.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentAComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
